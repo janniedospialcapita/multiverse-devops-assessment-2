3b21b6b3383dbf51869baae01476ae5f7d2deed8
Jan Niedospial <jan.niedospial@capita.com>
2023-02-24 16:05:32 +0000

    Completed ticket6. Some additional cleaning in the test file.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from extract import remove_duplicates
 from extract import remove_empty_lines
 from extract import capitalise_names
 from extract import validate_answer3
+from extract import save_output
 
 filename = "results.csv"
 
@@ -13,6 +14,7 @@ data = remove_duplicates(data)
 data = remove_empty_lines(data)
 data = capitalise_names(data)
 data = validate_answer3(data)
+save_output(data, 'clean_results.csv')
 
 for i in data:
     print(i)
