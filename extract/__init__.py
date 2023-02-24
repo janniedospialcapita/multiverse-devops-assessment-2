3aeb38e8ca6545a2a52f22ddb1a1c61f2a9592f8
Jan Niedospial <jan.niedospial@capita.com>
2023-02-24 12:27:32 +0000

    Completed ticket4

--- extract/__init__.py
+++ extract/__init__.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 def get_input(filename):
     
@@ -26,3 +27,25 @@ def remove_empty_lines(data):
             lines_not_empty.append(i)
 
     return lines_not_empty
+
+
+def remove_empty_lines(data):
+    lines_not_empty =[]
+    for i in data:
+        if len([x for x in i if x != '']) > 0:
+            lines_not_empty.append(i)
+
+    return lines_not_empty
+
+def capitalise_names(data):
+    def McOcase(match):
+        return match.group(1)+match.group(2).upper()
+
+    for i in data[1:]:
+        i[1] = i[1].capitalize()
+        i[2] = i[2].capitalize()
+        i[2] = re.sub(r"(\bMc)([a-z])", McOcase, i[2])
+        i[2] = re.sub(r"(\bO\')([a-z])", McOcase, i[2])
+        i[2] = re.sub(r"(\')([a-z])", McOcase, i[2])
+
+    return data
