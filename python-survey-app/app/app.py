3c02a89fc8878f1ebf1e31f1454efa18b5c65f5c
Jan Niedospial <jan.niedospial@capita.com>
2023-03-23 16:32:50 +0000

    Changed folder structure. Tidied up .gitignore.

--- python-survey-app/app/app.py
+++ python-survey-app/app/app.py
@@ -1,22 +1,16 @@
 import sqlite3 
 import sys
 
-from input import get_input
-from input import remove_duplicates
-from input import remove_empty_lines
-from input import capitalise_names
-from input import validate_answer3
-from input import save_output
-
+from input import *
 from sqldb import * 
 
-filename = "results.csv"
+filename = "./data/in/results.csv"
 
 def main(filename=None, data=None): 
     print(f'sqlite3 version: {sqlite3.sqlite_version}')
     print()
 
-    db = sqlite3.connect('results.db')
+    db = sqlite3.connect('./db/results.db')
 
     if data is None:
         data = get_input(filename)
@@ -36,7 +30,7 @@ def main(filename=None, data=None):
     data = remove_empty_lines(data)
     data = capitalise_names(data)
     data = validate_answer3(data)
-    save_output(data, 'clean_results.csv')
+    save_output(data, './data/out/clean_results.csv')
 
     create_table_clean_results(db)
     describe_table(db, 'clean_results')
